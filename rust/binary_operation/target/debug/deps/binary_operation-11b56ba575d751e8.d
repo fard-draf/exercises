5094d82749780dd0af9d1f6d4871b62134a95358
fard-draf <fard@ik.me>
2025-06-06 14:45:23 +0200

    update

--- rust/binary_operation/target/debug/deps/binary_operation-11b56ba575d751e8.d
+++
@@ -1,9 +0,0 @@
-/mnt/repo/dev/warehouse/playground/exercises/rust/binary_operation/target/debug/deps/libbinary_operation-11b56ba575d751e8.rmeta: src/main.rs Cargo.toml
-
-/mnt/repo/dev/warehouse/playground/exercises/rust/binary_operation/target/debug/deps/binary_operation-11b56ba575d751e8.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
