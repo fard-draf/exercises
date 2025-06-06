6f98a6aea492df20c13a3a7010c3d12ac2da29cd
fard-draf <fard@ik.me>
2025-06-06 15:02:12 +0200

    Restructure: rename to learning-systems-optimization and organize by domain

---
+++ blockchain-systems/pda-tracking/target/debug/deps/dead_reckoning-225d0c27d03977f8.d
@@ -0,0 +1,9 @@
+/mnt/repo/dev/warehouse/playground/exercises/rust/dead_reckoning/target/debug/deps/libdead_reckoning-225d0c27d03977f8.rmeta: src/main.rs Cargo.toml
+
+/mnt/repo/dev/warehouse/playground/exercises/rust/dead_reckoning/target/debug/deps/dead_reckoning-225d0c27d03977f8.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
