3b2b070ea1b05ed45b2b257e17cc04675c0086ed
fard-draf <fard@ik.me>
2025-07-14 13:44:26 +0200

    25W29D1_ended

--- weekly/week_29/src/main.rs
+++ weekly/week_29/src/main.rs
@@ -1,30 +1,46 @@
 mod day_1;
 
 fn main() {
-    let bit_buffer: u64 = 0b10101010;
-    println!("bit_buffer vierge: {:08b}", bit_buffer);
+    // let bit_buffer: u64 = 0b10101010;
+    // println!("bit_buffer vierge: {:08b}", bit_buffer);
 
 
-    let and = bit_buffer & 0b01010101;
-    println!("and: {:08b}", and);
+    // let and = bit_buffer & 0b01010101;
+    // println!("and: {:08b}", and);
 
-    let  or = bit_buffer | 0b01010101;
-    println!("or: {:08b}", or);
+    // let  or = bit_buffer | 0b01010101;
+    // println!("or: {:08b}", or);
 
-    let xor = bit_buffer ^ 0b01010101;
-    println!("xor: {:08b}", xor);
+    // let xor = bit_buffer ^ 0b01010101;
+    // println!("xor: {:08b}", xor);
 
-    let op_himself = !bit_buffer;
-    println!("!: {:08b}", op_himself);
+    // let op_himself = !bit_buffer;
+    // println!("!: {:08b}", op_himself);
 
-    let and_by_op_himself = bit_buffer & !bit_buffer;
-    println!("& and !: {:08b}", and_by_op_himself);
+    // let and_by_op_himself = bit_buffer & !bit_buffer;
+    // println!("& and !: {:08b}", and_by_op_himself);
 
-    let or_by_op_himself = bit_buffer | !bit_buffer;
-    println!("| and !: {:08b}", or_by_op_himself );
+    // let or_by_op_himself = bit_buffer | !bit_buffer;
+    // println!("| and !: {:08b}", or_by_op_himself );
 
-    let xor_by_op_himself = bit_buffer ^ !bit_buffer;
-    println!("^ and !: {:08b}", xor_by_op_himself);
+    // let xor_by_op_himself = bit_buffer ^ !bit_buffer;
+    // println!("^ and !: {:08b}", xor_by_op_himself);
+
+    
+    let data: u32 = 0xDACC5EFF;
+    let mask: u32 = 0x0000FF00;
+    println!("data hex: {:0X}", data);
+    println!("data:     {:032b}", data);
+    println!("mask:     {:032b}", mask);
+    let extract = data & mask;
+    println!("extract:  {:032b}", extract); 
+    let result = extract >> 8;
+    println!("binaire:  {:032b}", result);
+    println!("decimal:  {}", result);
+    println!("hexa:     {:X}", result);
+    
+
+
 
 }
 
--- weekly/week_29/target/debug/deps/week_29-cf8974bcc7039b3c.d
+++
@@ -1,8 +0,0 @@
-/mnt/repo/dev/warehouse/playground/exercises/weekly/week_29/target/debug/deps/week_29-cf8974bcc7039b3c.d: src/main.rs src/day_1/mod.rs src/day_1/ex1_compare_bits.rs src/day_1/ex2_toggle_bit.rs
-
-/mnt/repo/dev/warehouse/playground/exercises/weekly/week_29/target/debug/deps/week_29-cf8974bcc7039b3c: src/main.rs src/day_1/mod.rs src/day_1/ex1_compare_bits.rs src/day_1/ex2_toggle_bit.rs
-
-src/main.rs:
-src/day_1/mod.rs:
-src/day_1/ex1_compare_bits.rs:
-src/day_1/ex2_toggle_bit.rs:
