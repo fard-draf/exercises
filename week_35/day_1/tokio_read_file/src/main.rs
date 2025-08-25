234b9e65131f8ba394094e9ae4fca1919fb7a8df
fard-draf <fard@ik.me>
2025-08-25 17:18:07 +0200

    feat(tokio::select

--- week_35/day_1/tokio_read_file/src/main.rs
+++ week_35/day_1/tokio_read_file/src/main.rs
@@ -5,14 +5,22 @@ use tokio::io::{AsyncReadExt, BufReader};
 
 #[tokio::main]
 async fn main() {
+    let content1 = "test".repeat(100000);
+    let content2 = "test".repeat(100004);
+    tokio::fs::write("files/test1.json", &content1)
+        .await
+        .unwrap();
+    tokio::fs::write("files/test2.json", &content2)
+        .await
+        .unwrap();
     tokio::select! {
-        first = read_file("files/canboat1.json") => {
-            println!("First win: {first:?}");
-        }
+    _ = read_file("files/test1.json") => {
+                        println!("First win");
+                }
 
-        second = read_file("files/canboat2.json") => {
-            println!("Second win: {second:?}");
-        }
+    _ = read_file("files/test2.json") => {
+                        println!("second win");
+                }
     }
 }
 
@@ -21,7 +29,6 @@ async fn read_file(path: &str) -> Result<String, io::Result<()>> {
     let mut reader = BufReader::new(file);
     let mut buffer = Vec::new();
     reader.read_to_end(&mut buffer).await.unwrap();
-    let string = String::new();
 
     Ok(String::from_utf8_lossy(&buffer).to_string())
 }
